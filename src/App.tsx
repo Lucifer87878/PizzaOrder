7a498ab48fabe5cb627d07cc5df7b6e07e5e8c8c
Lucifer <nallenrockar123@gmail.com>
2024-03-28 10:04:49 +0100

    komenterade ut lite kod

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Button from "./Components/Button";
 import PizzaSizeDropdown from "./Components/PizzaSizeDropdown/PizzaSizeDropdown";
-import Topping from "./Components/Checkbox/Checkbox";
-import Checkbox from "./Components/Checkbox/Checkbox";
 import NavBar from "./Components/Navbar/Navbar";
-import ToppingsDropdown from "./Components/PizzaSizeDropdown/ToppingsDropdown";
+
+//--- Lämnar kvar dom i fall vi ändrar oss längre fram ----//
+
+// import { useState } from "react";
+// import Topping from "./Components/Checkbox/Checkbox";
+// import Checkbox from "./Components/Checkbox/Checkbox";
+// import ToppingsDropdown from "./Components/PizzaSizeDropdown/ToppingsDropdown";
 
 function App() {
   return (
