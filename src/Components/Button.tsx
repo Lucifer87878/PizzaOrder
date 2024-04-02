12cf4220463454825a961c421da01afa8c00409b
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-04-02 11:55:24 +0200

    button

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,10 +1,17 @@
 import "./sass/_buttons.scss";
+import React from "react";
 
-const Button = () => {
+interface ButtonProps {
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  text: string; // Använd en props för att ta emot texten för knappen
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, text }) => {
   return (
-    <>
-      <button className='CheckoutBtn'>Checkout</button>
-    </>
+    <button className='CheckoutBtn' onClick={onClick}>
+      {text}
+    </button>
   );
 };
+
 export default Button;
